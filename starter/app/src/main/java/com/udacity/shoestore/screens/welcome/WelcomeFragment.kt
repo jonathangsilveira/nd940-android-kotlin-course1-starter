5942d0e9415a5ee60d70dab2e68ed6eb7b0c44fa
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2020-12-14 22:22:32 -0300

    Welcome screen

--- starter/app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
@@ -1,11 +1,12 @@
 package com.udacity.shoestore.screens.welcome
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
@@ -25,7 +26,9 @@ class WelcomeFragment : Fragment() {
             false
         )
         binding.instructionsButton.setOnClickListener { view ->
-
+            view.findNavController().navigate(
+                WelcomeFragmentDirections.actionWelcomeFragmentToInstructionsFragment()
+            )
         }
         return binding.root
     }
